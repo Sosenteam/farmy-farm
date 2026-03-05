7da9f257e8944df1ebdd9b136b90c2d4b982033b
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2026-03-05 13:05:34 -0800

    Added some tick stuff

---
+++ grid_scripts/tile.gd
@@ -0,0 +1,7 @@
+@abstract
+class_name Tile
+
+var isOccupied:bool = false;
+
+@abstract
+func tick() -> void
---
+++ grid_scripts/tiles/example.gd
@@ -0,0 +1,4 @@
+extends Tile
+
+func tick() -> void:
+	print("I will print every tickHello!")

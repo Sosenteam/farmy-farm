660762ffc3f8b313757b34e0e9a0b2d1fc2dc583
Beckett Lynch <beckettlynch@gmail.com>
2026-03-05 21:48:31 -0800

    Sell Truck | Animations, Functions, Sprites
    Feel free to change, it's probably not how you want it, I was just bored so I did it anyways

---
+++ sell_truck.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+@onready var boxes_sprite = $Truck/Boxes
+@onready var truck_sprite = $Truck/Truck
+@onready var animation_player = $AnimationPlayer
+
+var items_in_truck = []
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_box_profile()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+# Adding an item to sell
+func add_item(item):
+	# item should be an item class so we can reference image, price, amount, etc.
+	items_in_truck.append(item)
+	
+func set_box_profile():
+	print("res://Assets/sell_truck/boxes/"+ str(items_in_truck.size()) +".png")
+	boxes_sprite.texture = load("res://Assets/sell_truck/boxes/"+ str(items_in_truck.size()) +".png")
+
+func send_off():
+	truck_sprite.texture = load("res://Assets/sell_truck/closed_truck.png")
+	animation_player.play("truck_leaving")
+	#sell_items function here
+	
+func come_back():
+	items_in_truck.clear() #might not be necesarry depending on how we sell items in future
+	truck_sprite.texture = load("res://Assets/sell_truck/closed_truck.png")
+	animation_player.play("truck_returning")
+	
+	
+	
+#temporary, just to test shwoing boxes
+func _on_static_body_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		add_item("ITEM")
+		set_box_profile()
+	if (items_in_truck.size() == 8 ):
+		send_off()
+		
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if (anim_name == "truck_returning"):
+		truck_sprite.texture = load("res://Assets/sell_truck/opened_truck.png")

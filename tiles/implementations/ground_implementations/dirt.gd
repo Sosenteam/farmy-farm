caa0ab177428ade57da40e8c12ec8cf1f6d4fbf9
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2026-03-10 08:44:01 -0700

    Fixed division by zero OH NO!!!!!!!!

--- tiles/implementations/ground_implementations/dirt.gd
+++ tiles/implementations/ground_implementations/dirt.gd
@@ -3,9 +3,9 @@ class_name Dirt extends Ground
 var moisture_percent:float
 
 # buffs
-var nitrogen:float
-var phosphorus:float
-var potassium:float 
+var nitrogen:float = 0
+var phosphorus:float = 0
+var potassium:float = 0 
 var growth_rate_multiplier:float = 1
 var yield_multiplier:float = 1
 var water_loss_multiplier:float = 1

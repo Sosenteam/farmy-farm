e24d1a6678a0c3f708bd7ccf4e6b1f525c806cb3
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2026-03-12 12:33:08 -0700

    Water

--- tiles/implementations/ground_implementations/dirt.gd
+++ tiles/implementations/ground_implementations/dirt.gd
@@ -11,7 +11,10 @@ var yield_multiplier:float = 1
 var water_loss_multiplier:float = 1
 
 func tick() -> void:
-	pass
+	moisture_percent = clampf(moisture_percent - (Tile.constants.BASE_WATER_LOSS_PER_TICK * water_loss_multiplier), 0.0, 1.0)
+	
+func change_water(water:float) -> void:
+	moisture_percent = clampf(moisture_percent + water, 0.0, 1.0)
 
 func change_nutrients(n:float, p:float, k:float):
 	nitrogen = clampf(nitrogen + n, 0, Tile.constants.nutrient_capacity)

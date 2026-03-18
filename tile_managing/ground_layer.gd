36e7e35148185258268e72716acab8e786ec3cde
Alec Singer <sosenteam@gmail.com>
2026-03-17 17:29:41 -0700

    fixed water rendering, made it so you can click w/o dragging

--- tile_managing/ground_layer.gd
+++ tile_managing/ground_layer.gd
@@ -8,11 +8,12 @@ func _ready() -> void:
 	pass
 
 func _use_tile_data_runtime_update(vector:Vector2i) -> bool:
-	return manager.ground_tiles_to_update.get(vector,false)
-	manager.ground_tiles_to_update = {}
+	var ret = manager.ground_tiles_to_update.get(vector,false)
+	return ret
+	
 func _tile_data_runtime_update(vect,tile_data):
 	var tile = map[tiles.vector_to_index(vect)]
-	var c = remap(tile.ground.moisture_percent,0,1,0,0.25)
+	var c = remap(tile.ground.moisture_percent,0,1,0,0.45)
 	var modulate_color = Color(1-c,1-c,1-c,1)
 	tile_data.modulate = modulate_color
 	return tile_data
--- tile_managing/input_manager.gd
+++ tile_managing/input_manager.gd
@@ -17,7 +17,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == 1:
 			mouse_pressed = event.pressed
-	if event is InputEventMouseMotion && mouse_pressed:
+	if (event is InputEventMouseMotion && mouse_pressed) || (event is InputEventMouseButton && event.button_index == 1 && event.pressed):
 		# Get tile where mouse pressed
 		var tilemap_pos = ground_layer.local_to_map(manager.get_local_mouse_position())
 		var index = tiles.vector_to_index(tilemap_pos)

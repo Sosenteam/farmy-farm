2e12d381718d333afe84ea50f3b944c83336caed
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2026-03-18 18:17:44 -0700

    Fixed issue where sprinkler would constantly max out water

--- tiles/implementations/occupant_implementations/machines/sprinkler.gd
+++ tiles/implementations/occupant_implementations/machines/sprinkler.gd
@@ -1,22 +1,23 @@
 class_name Sprinkler extends Machine
 
-var ticks_between_waterings:int = 320
-var ticks_since_last_watering:int = 310
-var ticks_to_saturate_dirt:int = 8
+var ticks_between_waterings:int = 250
+var ticks_since_last_watering:int = 0;
+var ticks_to_saturate_dirt:int = 12
 
 func _init():
 	machine_name = &"sprinkler"
-	ticks_since_last_watering = 310
+	ticks_since_last_watering = ticks_between_waterings - 10
 
 func tick() -> void:
-	#print(ticks_since_last_watering)
 	if ticks_since_last_watering > ticks_between_waterings:
 		var surroundingTiles = tile.tiles.get_surrounding_tiles(tile.index)
 		
-		for surrounding in surroundingTiles:
-			if surrounding:
-				if(surrounding.ground is Dirt):
-					surrounding.ground.change_water(0.1)
-					#print("watering " + str(surrounding.index))
+		if ticks_since_last_watering - ticks_between_waterings < (ticks_to_saturate_dirt+1):
+			for surrounding in surroundingTiles:
+				if surrounding:
+					if(surrounding.ground is Dirt):
+						surrounding.ground.change_water(1.0 / ticks_to_saturate_dirt)
+		else:
+			ticks_since_last_watering = 0
 	
 	ticks_since_last_watering += 1;

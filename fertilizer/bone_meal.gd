9c1b6d4d2f6d8fb989f37e3b13ee939604211c34
Alec Singer <sosenteam@gmail.com>
2026-03-19 14:17:00 -0700

    made basic fertilizer classes

---
+++ fertilizer/bone_meal.gd
@@ -0,0 +1,6 @@
+class_name BoneMeal extends Fertilizer
+
+func _init():
+	n_to_add = 10
+	p_to_add = 20
+	k_to_add = 0	
---
+++ fertilizer/seaweed_meal.gd
@@ -0,0 +1,6 @@
+class_name SeaweedMeal extends Fertilizer
+
+func _init():
+	n_to_add = 4
+	p_to_add = 0
+	k_to_add = 12
